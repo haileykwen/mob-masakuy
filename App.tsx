3d2fcd6f76fe7ecf809ecaeff92dea64ed36613b
Harli Ramli <harli.ramli@erasysconsulting.com>
2021-12-23 19:32:12 +0700

    slicing password recovery code screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Onboarding, PasswordRecovery, Signin, Signup, VerificationCode } from './src/screens';
+import { Onboarding, PasswordRecovery, PasswordRecoveryCode, Signin, Signup, VerificationCode } from './src/screens';
 
 const App = () => {
     return (
-        <PasswordRecovery />
+        <PasswordRecoveryCode />
     );
 }
 
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -3,11 +3,13 @@ import Signin from "./Signin";
 import Signup from "./Signup";
 import VerificationCode from "./VerificationCode";
 import PasswordRecovery from "./PasswordRecovery";
+import PasswordRecoveryCode from "./PasswordRecoveryCode";
 
 export {
     Onboarding,
     Signin,
     Signup,
     VerificationCode,
-    PasswordRecovery
+    PasswordRecovery,
+    PasswordRecoveryCode
 }
